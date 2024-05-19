a52549d13763ccc1573b577cd21e24af96d1e5ef
zp <bitporo@qq.com>
2024-05-19 20:50:55 +0800

    fix: 修改logo及ip获取问题

--- electron/service/system.js
+++ electron/service/system.js
@@ -9,9 +9,8 @@ class SystemService extends Service {
 
   async getIps() {
     const networkInterfaces = os.networkInterfaces()
-    return Object.keys(networkInterfaces).map(item => {
-      return networkInterfaces[item][1].address
-    })
+    // 获取网卡name，遍历网卡名字获取每个显卡ip数字，找到ip4地址返回
+    return Object.keys(networkInterfaces).map(item => networkInterfaces[item].find(ipItem => ipItem.family == 'IPv4').address)
   }
 }
 
