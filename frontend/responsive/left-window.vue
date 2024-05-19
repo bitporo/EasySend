60f436207ca016cf7318aaba8e735e21cbced77b
zp <bitporo@qq.com>
2024-05-19 22:52:39 +0800

    feat: 调整左右两边内容

--- frontend/responsive/left-window.vue
+++ frontend/responsive/left-window.vue
@@ -1,13 +1,19 @@
 <template>
   <view class="container">
-    <view>
-      感谢以下开源项目贡献：
-    </view>
-    <view>
-      electron-egg
-    </view>
-    <view>
-      uniapp
+    <view style="display: flex;flex-direction: column;justify-content: space-between;height: 100%;">
+      <view>
+      </view>
+      <view style="font-size: 12px;color: #686868;">
+        <view>
+          使用的技术实现：
+        </view>
+        <view>
+          electron-egg、uniapp
+        </view>
+        <view style="margin-top: 10px;">
+          Copyright © 2024 ZP. All Rights Reserved.
+        </view>
+      </view>
     </view>
   </view>
 </template>
@@ -16,7 +22,10 @@
 </script>
 
 <style scoped>
-  .container{
+  .container {
+    height: 100vh;
     padding: 20px;
+    box-sizing: border-box;
+    position: relative;
   }
 </style>
--- frontend/responsive/right-window.vue
+++ frontend/responsive/right-window.vue
@@ -1,11 +1,17 @@
 <template>
   <view class="right-content">
-    <view>
-      共享访问地址：
-    </view>
+    <view class="intro-title">共享访问地址：</view>
     <view v-for="ip in ipList">
-      <uni-link :href="`http://${ip}:7072`" :text="`http://${ip}:7072`"></uni-link>
+      <uni-link :href="`http://${ip}:7072`" :text="`http://${ip}:7072`" color="#007BFF"></uni-link>
     </view>
+    <view class="intro-title">反馈交流</view>
+    <view class="intro-content">QQ群: 3233634619</view>
+    <view class="intro-title">使用方法</view>
+    <view class="intro-content">使用浏览器访问“共享访问地址”即可</view>
+    <view class="intro-title">适用场景</view>
+    <view class="intro-content">局域网文文本文件共享、虚拟机与宿主机共享等</view>
+    <view class="intro-title">注意事项</view>
+    <view class="intro-content">本软件传送的任何文本文件均在本地，不获取上传任何用户数据至网络，请放心使用，但请注意谨慎发送敏感文本及文件（局域网任何人都可以访问），以及禁止用于违法犯罪活动，也概不承担使用本软件造成的损失责任问题，有使用问题及软件问题欢迎加入QQ群反馈</view>
   </view>
 </template>
 
@@ -39,6 +45,14 @@
 
 <style scoped>
   .right-content {
+    font-size: 14px;
     padding: 20px;
+  }
+  .intro-title{
+    padding: 10px 0;
+    font-weight: bold;
+  }
+  .intro-content{
+    color: #666666;
   }
 </style>
