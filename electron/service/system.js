1851cf12cc9dd19f9df0ec162f661134a639817f
bitporo <bitporo@qq.com>
2024-06-03 03:13:58 +0000

    fix: 优化ip获取问题

--- electron/service/system.js
+++ electron/service/system.js
@@ -10,7 +10,7 @@ class SystemService extends Service {
   async getIps() {
     const networkInterfaces = os.networkInterfaces()
     // 获取网卡name，遍历网卡名字获取每个显卡ip数字，找到ip4地址返回
-    return Object.keys(networkInterfaces).map(item => networkInterfaces[item].find(ipItem => ipItem.family == 'IPv4').address)
+    return Object.keys(networkInterfaces).map(item => networkInterfaces[item].find(ipItem => ipItem.family == 'IPv4')?.address).filter(item=>item)
   }
 }
 
