ca5385040e7213ee6959a521b1296a8aaeb710e9
bitporo <bitporo@qq.com>
2024-10-23 21:23:04 +0800

    chore: 1.4.1版本发布

--- frontend/pages/play/play.vue
+++ frontend/pages/play/play.vue
@@ -1,7 +1,7 @@
 <template>
   <view style="display: flex;flex-direction: column;height: 100vh;justify-content: center;">
     <view v-if="waiting" style="padding: 20px 0;text-align: center;">正在加载视频请稍等...</view>
-    <video v-if="playUrl" :src="playUrl" style="width: 100%;height: 50%;" @waiting="playWait" @loadedmetadata="playWait"></video>
+    <video v-if="playUrl" :src="playUrl" style="width: 100%;height: 100%;" @waiting="playWait" @loadedmetadata="playWait"></video>
   </view>
 </template>
 
